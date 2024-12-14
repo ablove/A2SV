4762516a877c31740a88023b0757c1e33fa9bf64
Abebe <119563686+ablove@users.noreply.github.com>
2024-12-14 15:59:09 -0800

    Time: 4 ms (51.23%) | Memory: 18 MB (18.47%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,5 +1,32 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers) - 1
+        while left < right :
+            sum = numbers[left] + numbers[right]
+            if sum == target:
+                return [left + 1, right + 1]
+                break
+            elif sum > target:
+                right -= 1
+            else:
+                left += 1
+        #Time: o(n)
+        # Space : o(1)
+
+        # n = len(numbers)-1
+        # l, r = 0, n-1
+
+
+        # while l < r :
+        #     summ = numbers[l] + numbers[r]
+        #     if summ == target:
+        #         return [l+1, r+1]
+        #         break
+        #     elif summ > target:
+        #         r -= 1
+        #     else:
+        #         l += 1
+                        
 
         # for i in range(len(numbers)-1):
         #     for j in range(i+1,len(numbers)):
@@ -11,23 +38,14 @@ class Solution:
 
         # optimized one
 
-        left, right = 0, len(numbers) - 1
+        # left, right = 0, len(numbers) - 1
 
-        while left < right :
-            sum = numbers[left] + numbers[right]
-            if sum == target:
-                return [left + 1, right + 1]
-                break
-            elif sum > target:
-                right -= 1
-            else:
-                left += 1
+        
 
 
 
         
-         # Time: o(n)
-         # Space : o(1)
+         
 
 
 
