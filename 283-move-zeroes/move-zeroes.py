5ee93e211f780ec215b62a751f9aaf310ee06c2a
Abebe <119563686+ablove@users.noreply.github.com>
2024-12-18 02:55:09 +0300

    Time: 5 ms (55.43%) | Memory: 18.9 MB (10.24%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,18 +1,32 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
+        placeholder = 0
+        for seeker in range(len(nums)):
+            if nums[seeker] != 0:
+                nums[seeker], nums[placeholder]= nums[placeholder], nums[seeker]
+                placeholder += 1
+
+
+
+
+        # seeker, placeholder = 0, 0
         # for i in nums:
         #     if i==0:
         #         nums.remove(i)
         #         nums.append(i)
         # here is another approach
-
-
         # the optimized one
-        placeholder=0
-        for seeker in range(len(nums)):
-            if nums[seeker]!= 0:
-                nums[placeholder],nums[seeker]=nums[seeker],nums[placeholder]
-                placeholder += 1
+
+       
+        
+
+
+
+
+
+        
+
+                
 
 
 
