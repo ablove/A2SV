83e1c533dec5b3e24487c6a5f26a9771d55f909c
Abebe <119563686+ablove@users.noreply.github.com>
2024-12-17 16:31:45 +0300

    Time: 101 ms (53.41%) | Memory: 28.5 MB (14.94%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,26 +1,70 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-       
-        n = len(height)
-        l = 0
-        r = n - 1
-        max_area = 0
+        l, r = 0, len(height)-1
+        Max = 0
 
         while l < r:
-            w = r - l
-            h = min(height[l], height[r])
-            a = w * h
-            max_area = max(max_area, a)
-            
-            if height[l] < height[r]:
-                l += 1
-            else:
+            min_height = min(height[l], height[r])
+            width = r-l
+            max_area = min_height * width
+            Max = max(Max, max_area)
+
+            if height[l] >= height[r]:
                 r -= 1
+            else:
+                l += 1    
 
-        return max_area
+        return Max  
 
-# Time Complexity: O(n)
-# Space Complexity: O(1)   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+#         n = len(height)
+#         l = 0
+#         r = n - 1
+#         max_area = 0
+
+#         while l < r:
+#             w = r - l
+#             h = min(height[l], height[r])
+#             a = w * h
+#             max_area = max(max_area, a)
+            
+#             if height[l] < height[r]:
+#                 l += 1
+#             else:
+#                 r -= 1
+
+#         return max_area
+
+# # Time Complexity: O(n)
+# # Space Complexity: O(1)   
 
 
 
