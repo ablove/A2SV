d0cd43106b191c8fefc6865ceb8c43ce6b8577cd
Abebe <119563686+ablove@users.noreply.github.com>
2024-12-18 01:56:40 +0300

    Time: 0 ms (100%), Space: 17.7 MB (10.95%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -5,12 +5,12 @@ class Solution:
         n = len(nums)
 
         while i < n:
-            if nums[i] == val:
-                nums[i] = nums[n - 1]
+            if nums[i] ==val:
+                nums[i] = nums[n-1]
                 n -= 1
             else:
                 i += 1
-        return n
+        return n            
 
         # Time: O(n)
         # Space: O(1)
