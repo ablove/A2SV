d75dbb76b54528c08abd56f291b0e385e87e4b56
Abebe <119563686+ablove@users.noreply.github.com>
2024-12-18 03:29:56 +0300

    Time: 0 ms (100%), Space: 18.8 MB (11.03%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        # Initialize the slow pointer
+        slow = 0
+
+        # Iterate with the fast pointer
+        for fast in range(1, len(nums)):
+            # If we find a new unique value
+            if nums[fast] != nums[slow]:
+                # Move the slow pointer forward
+                slow += 1
+                # Update the value at slow with the unique value
+                nums[slow] = nums[fast]
+
+        # Return the count of unique elements
+        return slow + 1
+
+        
+
+    
+        
+
+        
+        
