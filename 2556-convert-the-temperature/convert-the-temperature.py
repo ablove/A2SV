3bad92fe8c67e3672c20f63d1cb40e811a354be3
Abebe <119563686+ablove@users.noreply.github.com>
2025-01-31 03:59:23 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (14.67%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.py
+++ 2556-convert-the-temperature/convert-the-temperature.py
@@ -1,16 +1,87 @@
 class Solution:
     def convertTemperature(self, celsius: float) -> List[float]:
-        ans=[]
+        
         kel=celsius + 273.15
-        ans.append(kel)
+        
         fah=celsius * 1.80 + 32.00
-        ans.append(fah)
-        return(ans)
+        
+        return(kel, fah)
+
 
 
 
 
         
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
