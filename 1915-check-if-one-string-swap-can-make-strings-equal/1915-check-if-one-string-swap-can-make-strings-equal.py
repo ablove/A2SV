0ee20cfd972716679f6cb70aae137dffa72ede1e
Abebe <119563686+ablove@users.noreply.github.com>
2025-02-05 18:47:59 +0300

    Time: 0 ms (100%), Space: 17.9 MB (27.73%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.py
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.py
@@ -4,12 +4,10 @@ class Solution:
         store_s1 = set()
         store_s2 = set()
         for i in range(len(s1)):
-            for j in range(len(s2)):
-
-                if i == j and s1[i] != s2[j]:
+                if s1[i] != s2[i]:
                     count += 1
                     store_s1.add(s1[i])
-                    store_s2.add(s2[j])
+                    store_s2.add(s2[i])
         if count == 0 or count ==2 and store_s1 == store_s2:
             return True
         else:
