9a9a0b24b859cde188ad16fba0674697f9890e60
Abebe <119563686+ablove@users.noreply.github.com>
2025-02-05 06:50:18 +0300

    Time: 5 ms (38.34%), Space: 19.7 MB (5.9%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -11,8 +11,11 @@ class Solution:
             if num == diff and len(count[num]) > 1:
                 return [count[num][0], count[num][1]]
                 
-            if (diff) in count and num != diff:
+            elif diff in count and num != diff:
                 return [count[num][0], count[diff][0]]
+               
+
+
 
 
 
