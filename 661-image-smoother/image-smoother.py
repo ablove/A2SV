f952924150102189578c61132ceb11347fd7d955
Abebe <119563686+ablove@users.noreply.github.com>
2025-02-06 18:22:31 +0300

    Time: 231 ms (15.91%) | Memory: 18.6 MB (81.28%) - LeetSync

---
+++ 661-image-smoother/image-smoother.py
@@ -0,0 +1,45 @@
+class Solution:
+    def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
+
+        rows, cols = len(img), len(img[0])
+
+        ans = [[0] * cols for i in range(rows)]
+
+        def inbound(x,y):
+            return 0 <= x < rows and 0<= y < cols
+
+        for i in range(rows):
+            for j in range(cols):
+                arr =[(i-1,j),(i+1,j),(i,j-1),(i,j+1),(i-1,j-1),(i-1,j+1),(i+1,j-1),(i+1, j+1)]
+
+
+                count, summ = 1, img[i][j]
+                for val in arr:
+
+                    x,y = val[0], val[1]
+
+                    if inbound(x,y):
+                        count += 1
+                        summ += img[x][y]
+                ans[i][j] = (summ//count)
+        return ans
+
+
+
+
+
+        
+
+
+
+
+
+                
+                
+
+
+
+
+
+
+        
