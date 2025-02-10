62412311939f7375c9e54e9157cba75dac6a8724
Abebe <119563686+ablove@users.noreply.github.com>
2025-02-10 06:44:37 +0300

    Time: 7 ms (10.37%) | Memory: 23 MB (77.22%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -6,9 +6,11 @@ class Solution:
         left, right =0, len(s)-1
 
         while left<right:
+
             s[left], s[right]= s[right], s[left]
             left += 1
             right -= 1
+            
         return s    
 
 
