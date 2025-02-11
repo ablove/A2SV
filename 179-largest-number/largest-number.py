2f0e1dd2594290f711e795c88e0f3cdd64c6f9a9
Abebe <119563686+ablove@users.noreply.github.com>
2025-02-11 18:07:07 +0300

    Time: 12 ms (6.12%) | Memory: 17.6 MB (79.48%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -1,25 +1,6 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
 
-        str_nums = [str(i) for i in nums]
-        print(str_nums)
-        
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         nums=[str(i) for i in nums]
 
         for i in range(len(nums)):
