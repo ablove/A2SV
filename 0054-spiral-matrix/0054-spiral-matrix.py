ec4b87f8b86acbe6fca4abca639de67797083770
Abebe <119563686+ablove@users.noreply.github.com>
2025-02-11 21:47:58 +0300

    Time: 0 ms (100%), Space: 17.8 MB (57.99%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,25 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+
+        result = []
+
+        while matrix:
+            result += matrix.pop(0)  
+            
+            if matrix and matrix[0]:  
+                matrix = list(zip(*matrix))[::-1]
+
+        return result 
+
+
+
+
+
+
+
+
+
+
+
+
+        
