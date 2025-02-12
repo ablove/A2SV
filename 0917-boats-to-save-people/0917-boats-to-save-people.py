2e278469e64accc1f94792256c1ba0ad33fe9958
Abebe <119563686+ablove@users.noreply.github.com>
2025-02-12 15:00:19 +0300

    Time: 44 ms (83.88%), Space: 23.4 MB (21.59%) - LeetHub

--- 0917-boats-to-save-people/0917-boats-to-save-people.py
+++ 0917-boats-to-save-people/0917-boats-to-save-people.py
@@ -1,20 +1,66 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
 
-    # Sort the weights of people
         people.sort()
-        left, right = 0, len(people) - 1
-        boats = 0
+        left, right = 0, len(people)-1
+        boat_count = 0
 
         while left <= right:
-        # If the lightest and heaviest person can share a boat
-            if people[left] + people[right] <= limit:
-                left += 1  # Move the left pointer inward
-        # The heaviest person always gets a boat
-            right -= 1
-            boats += 1  # Increment boat count
+            summ = people[left] + people[right]
+
+            if summ >limit:
+                boat_count += 1
+                right -= 1
+
+            else:
+                boat_count += 1
+                left += 1
+                right -= 1
+        return boat_count            
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # # Sort the weights of people
+    #     people.sort()
+    #     left, right = 0, len(people) - 1
+    #     boats = 0
+
+    #     while left <= right:
+    #     # If the lightest and heaviest person can share a boat
+    #         if people[left] + people[right] <= limit:
+    #             left += 1  # Move the left pointer inward
+    #     # The heaviest person always gets a boat
+    #         right -= 1
+    #         boats += 1  # Increment boat count
     
-        return boats
+    #     return boats
+
+
+
+
 
             
 
