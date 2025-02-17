5778f908ccaa50eb5fb9f9e5bb744b1e5233cc60
Abebe <119563686+ablove@users.noreply.github.com>
2025-02-18 02:23:44 +0300

    Time: 64 ms (41.85%), Space: 42 MB (7.61%) - LeetHub

---
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+
+        n = len(s)
+        prefix = [0] * (n + 1)  # Extra space to avoid out-of-bounds
+
+        # Step 1: Apply the shifts using difference array technique
+        for shift in shifts:
+            start, end, direction = shift
+            if direction == 0:
+                prefix[start] -= 1
+                if end + 1 < n:
+                    prefix[end + 1] += 1
+            else:
+                prefix[start] += 1
+                if end + 1 < n:
+                    prefix[end + 1] -= 1  
+
+        for j in range(1, n):
+            prefix[j] += prefix[j - 1]
+
+        ans = []
+        for k in range(n):
+            new_letter = ((ord(s[k]) - ord('a') + prefix[k]) % 26) + ord('a')
+            ans.append(chr(new_letter))
+
+        return "".join(ans)   
+
