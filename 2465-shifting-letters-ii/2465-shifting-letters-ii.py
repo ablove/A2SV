5a4f32641748567550cf76c40b99e88bd31e803d
Abebe <119563686+ablove@users.noreply.github.com>
2025-02-18 02:42:00 +0300

    Time: 63 ms (49.02%), Space: 41.9 MB (10.94%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.py
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.py
@@ -6,17 +6,18 @@ class Solution:
 
         for shift in shifts:
             start, end, direction = shift
+
             if direction == 0:
-                prefix[start] -= 1
+                prefix[start] += -1
                 if end + 1 < n:
                     prefix[end + 1] += 1
             else:
                 prefix[start] += 1
                 if end + 1 < n:
-                    prefix[end + 1] -= 1  
+                    prefix[end + 1] += -1        
 
-        for j in range(1, n):
-            prefix[j] += prefix[j - 1]
+        for j in range(1, len(prefix)):
+            prefix[j] += prefix[j-1]
 
         ans = []
         for k in range(n):
