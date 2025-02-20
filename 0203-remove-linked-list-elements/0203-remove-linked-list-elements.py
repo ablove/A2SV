e31890cfcdcbf1dd142cf604d3ebec14783d3bf4
Abebe <119563686+ablove@users.noreply.github.com>
2025-02-20 09:51:44 +0300

    Time: 0 ms (100%), Space: 19.7 MB (16.06%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -11,16 +11,21 @@ class Solution:
         dummy.next = head
         
         prev = dummy
-        curr = head  
-        
+        curr = head 
+
         while curr:
+
             if curr.val == val:
                 prev.next = curr.next
             else:
                 prev = curr
             curr = curr.next
+            
+        return dummy.next            
+
+
         
-        return dummy.next
+       
 
                 
                         
