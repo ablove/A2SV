f90552a77017c73d343e21a0daf034ca4fe7c916
Abebe <119563686+ablove@users.noreply.github.com>
2025-02-21 10:26:32 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (53.76%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -1,21 +1,48 @@
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         
+        # n = len(candies)
+        # ans = [] * n
+        # for i in range(n):
+        #     candies[i] += extraCandies
+        #     val = candies[i]
+        #     for j in range(n):
+        #         if candies[j] > val:
+        #             # print(candies[i] , i ,val)
+        #             ans.append(False)
+        #             break
+        #     else:
+        #         ans.append(True)
+        #     candies[i] -= extraCandies    
+        #     # print(candies)
+        # return ans 
+
+
+        ## Optimized One
+
         n = len(candies)
         ans = [] * n
+        max_num = max(candies)
+
         for i in range(n):
             candies[i] += extraCandies
-            val = candies[i]
-            for j in range(n):
-                if candies[j] > val:
-                    # print(candies[i] , i ,val)
-                    ans.append(False)
-                    break
-            else:
+            if candies[i] >= max_num:
                 ans.append(True)
-            candies[i] -= extraCandies    
-            # print(candies)
-        return ans           
+            else:    
+                ans.append(False)
+        return ans        
+
+
+
+
+
+         
+
+
+
+
+
+
 
 
 
