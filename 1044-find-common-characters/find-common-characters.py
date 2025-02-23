a278c8d9b54150fabb5d423d86ec66aaf77b5ee7
Abebe <119563686+ablove@users.noreply.github.com>
2025-02-23 03:04:16 +0300

    Time: 11 ms (61.25%) | Memory: 17.9 MB (66.09%) - LeetSync

--- 1044-find-common-characters/find-common-characters.py
+++ 1044-find-common-characters/find-common-characters.py
@@ -1,19 +1,22 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        answer =[]
-        minWord = words[0]
-        for s in words:
-            minWord = minWord if len(minWord)<=len(s) else s
-        for char in minWord:
-            flag = True 
-            for i, check in enumerate(words):
-                if char not in check:
-                    flag = False 
-                else:
-                    words[i] = check.replace(char, '', 1)
-            if flag :
-               answer.append(char)
-        return answer 
+
+        common_count = Counter(words[0])
+        
+        for word in words[1:]:
+            word_count = Counter(word)
+            common_count &= word_count 
+        
+        result = []
+        for char, count in common_count.items():
+            result.extend([char] * count)  
+        
+        return result    
+        
+        
+
+
+        
             
           
 
@@ -22,37 +25,4 @@ class Solution:
 
 
 
-
-
-
-
-
-
-
-        
-        
-
-
-            
-
-
-
-
-
-
-
-
-            
-
-
-        
-
     
-
-
-
-
-
-
-
-        
