c5478521bf86be9bf7e7ab1e62f620fd029e8639
Abebe <119563686+ablove@users.noreply.github.com>
2025-03-06 04:53:27 +0300

    Time: 3 ms (24.59%) | Memory: 18.2 MB (10.04%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.py
+++ 797-rabbits-in-forest/rabbits-in-forest.py
@@ -1,13 +1,9 @@
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
-
-        count = Counter(answers) 
+        count = Counter(answers)
         total_rabbits = 0
-
-        for x, y in count.items():
-            
-            groups = math.ceil(y / (x + 1)) 
-            total_rabbits += groups * (x + 1) 
-
-        return total_rabbits
         
+        for k, freq in count.items():
+            total_rabbits += math.ceil(freq / (k + 1)) * (k + 1)
+        
+        return total_rabbits
