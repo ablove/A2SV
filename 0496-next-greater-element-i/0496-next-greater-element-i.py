ee9f4e4093790ae0dac4469fe5a25d4dbb8e6b68
Abebe <119563686+ablove@users.noreply.github.com>
2025-03-06 17:45:52 +0300

    Time: 0 ms (100%), Space: 18 MB (40.58%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -0,0 +1,26 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+
+        stack = []
+        track = {}
+        for num in nums2:
+            while stack and stack[-1] < num:
+                val = stack.pop()
+                track[val] = num
+            stack.append(num)
+
+        ans = []
+        for num in nums1:
+            if num in track:
+                ans.append(track[num])
+            else:
+                ans.append(-1) 
+        return ans           
+
+
+
+
+
+
+
+        
