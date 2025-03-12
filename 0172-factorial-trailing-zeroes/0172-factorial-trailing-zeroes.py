18de93c0fc7adfbc9665d24351aef4d63292fcc5
Abebe <119563686+ablove@users.noreply.github.com>
2025-03-12 06:56:15 +0300

    Time: 0 ms (100%), Space: 17.7 MB (81.58%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -1,23 +1,8 @@
-
 class Solution:
     def trailingZeroes(self, n: int) -> int:
-        count = 0
-        while n >= 5:
-            n //= 5
-            count += n
-        return count
+        if n < 5:
+            return 0
+        return (n // 5) + self.trailingZeroes(n // 5)
 
-               
-
-
-        
-
-
-
-
-
-        
-
-
-
-
+solution = Solution()
+print(solution.trailingZeroes(25))  # Output: 6
