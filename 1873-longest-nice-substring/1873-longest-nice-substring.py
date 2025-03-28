513f5eac476e24e1e7c2a6e67621eea6ce143e59
Abebe <119563686+ablove@users.noreply.github.com>
2025-03-28 18:23:10 +0300

    Time: 4 ms (56.98%), Space: 17.7 MB (63.69%) - LeetHub

---
+++ 1873-longest-nice-substring/1873-longest-nice-substring.py
@@ -0,0 +1,31 @@
+class Solution:
+    def longestNiceSubstring(self, s: str) -> str:
+
+        def divide(s):
+            # base:
+            if len(s) <= 1:
+                return ""
+            set_s = set(s)
+            for i, char in enumerate(s):
+                opposite_case = char.swapcase()
+
+                if opposite_case not in set_s:
+                    left = divide(s[:i])
+                    right = divide(s[i+1:])
+
+                    if len(left) >= len(right):
+                        return left
+                    return right
+            return s        
+        return divide(s)          
+
+
+
+
+
+
+
+
+
+
+        
