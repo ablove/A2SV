102c03b8ac971eb66a60923e06747653e04fd595
Abebe <119563686+ablove@users.noreply.github.com>
2025-03-31 12:05:24 +0300

    Time: 17 ms (6.13%) | Memory: 18.7 MB (52.40%) - LeetSync

--- 859-design-circular-deque/design-circular-deque.py
+++ 859-design-circular-deque/design-circular-deque.py
@@ -49,13 +49,3 @@ class MyCircularDeque:
     def isFull(self) -> bool:
         return self.size == self.capacity
 
-# Your MyCircularDeque object will be instantiated and called as such:
-# obj = MyCircularDeque(k)
-# param_1 = obj.insertFront(value)
-# param_2 = obj.insertLast(value)
-# param_3 = obj.deleteFront()
-# param_4 = obj.deleteLast()
-# param_5 = obj.getFront()
-# param_6 = obj.getRear()
-# param_7 = obj.isEmpty()
-# param_8 = obj.isFull()
