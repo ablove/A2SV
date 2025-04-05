4912269d245130e29cc2f973ed47d592ee502d8a
Abebe <119563686+ablove@users.noreply.github.com>
2025-04-05 03:31:44 +0300

    Time: 64 ms (5.24%), Space: 26.9 MB (94.88%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -1,16 +1,25 @@
+# class Solution:
+#     def findDuplicates(self, nums: List[int]) -> List[int]:
+
+#         store = Counter(nums)
+#         ans = []
+#         for i in store: 
+#             if store[i] == 2:
+#                 ans.append(i)
+#         return ans        
+
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
+        i = 0
+        while i < len(nums):
+            correct = nums[i] - 1
+            if nums[i] != nums[correct]:
+                nums[i], nums[correct] = nums[correct], nums[i]
+            else:
+                i += 1
 
-        store = Counter(nums)
-        ans = []
-
-        for i in store:
-            
-            if store[i] == 2:
-                ans.append(i)
-
-        return ans        
-
-
-
-        
+        result = []
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                result.append(nums[i])
+        return result
